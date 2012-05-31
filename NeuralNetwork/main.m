c887f72c3a1e7c437dd640860ecd240dafa9caf3
Ivan Ushakov <Ivan.MoLoT@gmail.com>
2012-05-31 09:54:12 +0400

    Initial Commit

---
+++ NeuralNetwork/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  NeuralNetwork
+//
+//  Created by Иван Ушаков on 31.05.12.
+//  Copyright (c) 2012 ООО Скрипт. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
