1aa2a630ee0f12bc24c4fa720861e80ea0b5e922
Ivan Ushakov <Ivan.MoLoT@gmail.com>
2012-06-01 22:48:13 +0400

    add learning and test

--- NeuralNetwork/main.m
+++ NeuralNetwork/main.m
@@ -21,15 +21,47 @@ float* imageFileToArray(NSString *path);
 int main(int argc, const char * argv[])
 {
 	@autoreleasepool {
-		
-		NSString  *inputPath = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/input.png"];
-		float *array = imageFileToArray(inputPath);
-		
 		NeuralNetwork *network = [[NeuralNetwork alloc] initWithNumInputs:336 numHiddenNeurons:130 numOutputs:10];
 		
-		[network backwardPropagationWithInput:array result:9];
-		NSLog(@"%ld", [network forwardPropagationWithInput:array]);
+		NSString *path = [NSString stringWithFormat:@"%@/numbers", [[NSFileManager defaultManager] currentDirectoryPath]];
+		NSURL *directoryURL = [NSURL fileURLWithPath:path];
+		NSDirectoryEnumerator *enumerator = [[NSFileManager defaultManager] enumeratorAtURL:directoryURL
+																 includingPropertiesForKeys:NULL
+																					options:NSDirectoryEnumerationSkipsSubdirectoryDescendants
+																			   errorHandler:^(NSURL *url, NSError *error) {return YES;}];
+		for (NSURL *url in enumerator) { 
+			NSInteger result = [[url lastPathComponent] integerValue];
+			NSDirectoryEnumerator *enumerator2 = [[NSFileManager defaultManager] enumeratorAtURL:url
+																	 includingPropertiesForKeys:NULL
+																						options:NSDirectoryEnumerationSkipsHiddenFiles
+																				   errorHandler:^(NSURL *url, NSError *error) {return YES;}];
+			for (NSURL *url in enumerator2) { 
+				float *array = imageFileToArray([url path]);
+				[network backwardPropagationWithInput:array result:result];
+
+			}
+		}
 		
+		NSString  *inputPath = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/0.png"];
+		NSLog(@"%ld", [network forwardPropagationWithInput:imageFileToArray(inputPath)]);
+		inputPath = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/1.png"];
+		NSLog(@"%ld", [network forwardPropagationWithInput:imageFileToArray(inputPath)]);
+		inputPath = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/2.png"];
+		NSLog(@"%ld", [network forwardPropagationWithInput:imageFileToArray(inputPath)]);
+		inputPath = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/3.png"];
+		NSLog(@"%ld", [network forwardPropagationWithInput:imageFileToArray(inputPath)]);
+		inputPath = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/4.png"];
+		NSLog(@"%ld", [network forwardPropagationWithInput:imageFileToArray(inputPath)]);
+		inputPath = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/5.png"];
+		NSLog(@"%ld", [network forwardPropagationWithInput:imageFileToArray(inputPath)]);
+		inputPath = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/6.png"];
+		NSLog(@"%ld", [network forwardPropagationWithInput:imageFileToArray(inputPath)]);
+		inputPath = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/7.png"];
+		NSLog(@"%ld", [network forwardPropagationWithInput:imageFileToArray(inputPath)]);
+		inputPath = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/8.png"];
+		NSLog(@"%ld", [network forwardPropagationWithInput:imageFileToArray(inputPath)]);
+		inputPath = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/9.png"];
+		NSLog(@"%ld", [network forwardPropagationWithInput:imageFileToArray(inputPath)]);
 		[network release];
 	}
     return 0;
